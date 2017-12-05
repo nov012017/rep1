5256597ab4109b3f7b1aa95e9e5183bb8e78a54f
Mounika <mounikapabba@gmail.com>
2017-12-05 12:20:39 +0530

    Test File

---
+++ mymvnproj/src/test/java/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+
+	public void f1()
+	{
+		System.out.println("hello how are you");
+	}
+	public static void main(String argv[])
+	{
+		System.out.println("hello");
+	}
+	
+}
